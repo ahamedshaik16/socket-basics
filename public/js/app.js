afe79b50a354fd4abadb230dd794876de45dd81d
Shuhail Ahamed <sohailcourses@gmail.com>
2016-12-22 02:08:15 +0530

    Add message to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -8,4 +8,17 @@ socket.on('connect', function () {
 socket.on('message', function (message) {
 	console.log('New message:');
 	console.log(message.text);
+
+	jQuery('.messages').append('<p>'+ message.text+'</p>');
+});
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function() {
+	event.preventDefault();
+	var $message = $form.find('input[name=message]');
+	socket.emit('message', {
+		text : $message.val()
+	});
+	$message.val('');
 });
